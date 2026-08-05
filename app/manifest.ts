a4bb8b0d5055929d1424e56857e71425ace6f9d4
manubonf <manuele.bonfanti@gmail.com>
2026-08-05 11:06:38 +0200

    prova icone pwa

--- app/manifest.ts
+++ app/manifest.ts
@@ -20,13 +20,7 @@ export default function manifest(): MetadataRoute.Manifest {
         sizes: '512x512',
         type: 'image/png',
         purpose: 'any',
-      },
-      {
-        src: '/icons/icon-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'maskable',
-      },
+      }
     ],
   }
 }
